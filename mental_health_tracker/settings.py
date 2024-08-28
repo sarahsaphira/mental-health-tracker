241b7d8f2cf298d8b9a22e2e47b9d475cd148133
sarahsaphira <sarahsaphira80@gmail.com>
2024-08-28 16:53:23 +0700

    commit

--- mental_health_tracker/settings.py
+++ mental_health_tracker/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-@9r=-5psv(!h@u0hj$q&f^j=f8&oi%jg3i2c_tp24ak95v)6i2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1", "sarah-saphira31-mentalhealthtracker.pbp.cs.ui.ac.id."]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "sarah-saphira31-mentalhealthtracker.pbp.cs.ui.ac.id"]
 
 
 # Application definition
