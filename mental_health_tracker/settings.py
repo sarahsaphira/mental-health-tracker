047b577c22e0aaec57d9f820bdec2829b4910494
sarahsaphira <sarahsaphira80@gmail.com>
2024-09-18 13:10:04 +0700

    Tutorial 3

--- mental_health_tracker/settings.py
+++ mental_health_tracker/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,7 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-@9r=-5psv(!h@u0hj$q&f^j=f8&oi%jg3i2c_tp24ak95v)6i2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+PRODUCTION = os.getenv("PRODUCTION", False)
+DEBUG = not PRODUCTION
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "sarah-saphira31-mentalhealthtracker.pbp.cs.ui.ac.id"]
 
@@ -122,4 +124,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-CSRF_TRUSTED_ORIGINS = ["http://localhost","http://127.0.0.1","http://https://sarah-saphira31-mentalhealthtracker.pbp.cs.ui.ac.id/"]
+CSRF_TRUSTED_ORIGINS = ["http://localhost","http://127.0.0.1","http://sarah-saphira31-mentalhealthtracker.pbp.cs.ui.ac.id/", "https://sarah-saphira31-mentalhealthtracker.pbp.cs.ui.ac.id/"]
